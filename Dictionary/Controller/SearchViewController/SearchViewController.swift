25b57f0f3fab324644df043bb4e681c04f98ca15
Anıl Avcı <anilavci52@gmail.com>
2023-02-14 22:23:27 +0300

    keyboard hide ok

--- Dictionary/Controller/SearchViewController/SearchViewController.swift
+++ Dictionary/Controller/SearchViewController/SearchViewController.swift
@@ -12,13 +12,71 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchView: UIView!
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var searchTableView: UITableView!
+    @IBOutlet weak var loginButtonBottomConstraint: NSLayoutConstraint!
     private var cell = "searchCell"
     var searchArray = [String]()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchTableView.register(UINib(nibName: "SearchTableViewCell", bundle: nil), forCellReuseIdentifier: cell)
         setupDelegate()
         searchTextView()
+        keyboardNotification()
+        searchTableView.backgroundColor = .white
+    }
+    //MARK: -   Notifications for when the keyboard opens/closes
+    private func keyboardNotification() {
+        self.searchTextField.delegate = self
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        // Move the view only when the usernameTextField or the passwordTextField are being edited
+        if searchTextField.isEditing {
+            moveViewWithKeyboard(notification: notification, viewBottomConstraint: self.loginButtonBottomConstraint, keyboardWillShow: true)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        moveViewWithKeyboard(notification: notification, viewBottomConstraint: self.loginButtonBottomConstraint, keyboardWillShow: false)
+    }
+    
+    func moveViewWithKeyboard(notification: NSNotification, viewBottomConstraint: NSLayoutConstraint, keyboardWillShow: Bool) {
+        // Keyboard's size
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        let keyboardHeight = keyboardSize.height
+        
+        // Keyboard's animation duration
+        let keyboardDuration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        
+        // Keyboard's animation curve
+        let keyboardCurve = UIView.AnimationCurve(rawValue: notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! Int)!
+        
+        // Change the constant
+        if keyboardWillShow {
+            let safeAreaExists = (self.view?.window?.safeAreaInsets.bottom != 0) // Check if safe area exists
+            let bottomConstant: CGFloat = 20
+            viewBottomConstraint.constant = keyboardHeight + (safeAreaExists ? 0 : bottomConstant)
+        }else {
+            viewBottomConstraint.constant = 20
+        }
+        // Animate the view the same way the keyboard animates
+        let animator = UIViewPropertyAnimator(duration: keyboardDuration, curve: keyboardCurve) { [weak self] in
+            // Update Constraints
+            self?.view.layoutIfNeeded()
+        }
+        // Perform the animation
+        animator.startAnimation()
     }
     private func setupDelegate() {
         searchTableView.delegate = self
@@ -40,6 +98,7 @@ class SearchViewController: UIViewController {
             print("Arama fonksiyonunda veri çekilirken bir sorunla karşılaştı")
         }
     }
+    //MARK: - SearchButton
     @IBAction func searchButtonPressed(_ sender: Any) {
         if !searchTextField.text.isNilOrEmpty {
             searchArray.append(searchTextField.text!)
@@ -47,30 +106,25 @@ class SearchViewController: UIViewController {
             performSegue(withIdentifier: "searchtoDetail", sender: nil)
         } else {
             DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            self.showAlertView(title: "Error!", message: "You should write a word...", alertActions: [])
+                guard let self = self else { return }
+                self.showAlertView(title: "Error!", message: "You should write a word...", alertActions: [])
             }
         }
     }
-    
+    //MARK: - Segue to SearchDetail
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       
-            if segue.identifier == "searchtoDetail" {
-                let gidilecekVC = segue.destination as! SearchDetailViewController
-                gidilecekVC.searchText = searchTextField.text!
-                
-            }
-
+        if segue.identifier == "searchtoDetail" {
+            let gidilecekVC = segue.destination as! SearchDetailViewController
+            gidilecekVC.searchText = searchTextField.text!
+        }
     }
 }
 // MARK: - Table view data source
 extension SearchViewController: UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchArray.count == nil ? 0 : searchArray.prefix(5).count
-        
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = searchTableView.dequeueReusableCell(withIdentifier: cell, for:  indexPath) as! SearchTableViewCell
         cell.searchLabel.text = searchArray.reversed()[indexPath.row]
         return cell
@@ -83,6 +137,11 @@ extension SearchViewController: UITableViewDelegate,UITableViewDataSource {
         vc?.searchText = searchArray.reversed()[indexPath.row]
         self.navigationController?.pushViewController(vc!, animated: true)
     }
-    
 }
-
+extension SearchViewController: UITextFieldDelegate {
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Close keyboard when you press 'return'
+        textField.resignFirstResponder()
+        return true
+    }
+}
